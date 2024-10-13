6112ea3091cec425625b8274376d0b6dc70f7823
MiguelAmorim09 <duguerel@gmail.com>
2024-10-13 19:47:16 -0300

    Todos os topicos de estatisticas terminados

--- Manutencao.java
+++ Manutencao.java
@@ -4,13 +4,14 @@ import static java.lang.System.*;
 
 public class Manutencao {
     static Scanner leitor = new Scanner(in);
-    enum Ordens {porRa, porNome, porCurso, porMedia};
+    enum Ordens {porRa, porNome, porCurso, porMedia}
     private static Ordens ordemAtual = Ordens.porRa;
     private static ManterEstudantes estuds = new ManterEstudantes();
     private static String[] disciplinas;
-    private static int qtasDisci;
+    private static int qtasDisci, indEstudMenorNota, indEstudMaiorNota;
     private static int[] maioresAprovacoes, maioresRetencoes;
     private static double[] somasNotas;
+    private static double notaMaisBaixa, notaMaisAlta;
     // índice resultante da pesquisa binária
 
     public static void main(String[] args) throws Exception {
@@ -123,51 +124,66 @@ public class Manutencao {
     }
 
     private static void estatisticas(){
-        out.println("Estatisticas dos alunos");
+        out.println("\nEstatisticas dos alunos");
         out.print("Digite o nome do arquivo para ser lido: ");
         String nomeArquivo = leitor.nextLine();
         lerDisciplinas(nomeArquivo);
+        int indMaisAprov = 0, indMaisReten = 0, indEstudMaiorMedia = 0, indDisciMenorMedia = 0;
+        double maiorMediaTemp, maiorMediaEstud = 0, menorMediaDisci = 10;
         maioresAprovacoes = new int[15];
         maioresRetencoes = new int[15];
         somasNotas = new double[15];
-        int indMaisAprov = 0, indMaisReten = 0, indEstudMaior = 0, indDisciMaior = 0;
-        double mediaEstud, mediaDisci, maiorEstud = 0, menorDisci = 10;
-        for(int indEstu = 0; indEstu < estuds.qtosDados; indEstu++){
-            maisAprovacoes(estuds.dados[indEstu]);
+        for(int indEstud = 0; indEstud < estuds.qtosDados; indEstud++){
+            maisAprovacoes(estuds.dados[indEstud]);
             for(int indAprov = 0; indAprov < qtasDisci; indAprov++){
-                if(maioresAprovacoes[indAprov] > indMaisAprov)
+                if(maioresAprovacoes[indAprov] >= indMaisAprov)
                     indMaisAprov = indAprov;
             }
 
-            maisRetencoes(estuds.dados[indEstu]);
+            maisRetencoes(estuds.dados[indEstud]);
             for(int indReten = 0; indReten < qtasDisci; indReten++){
-                if(maioresRetencoes[indReten] > indMaisReten)
+                if(maioresRetencoes[indReten] >= indMaisReten)
                     indMaisReten = indReten;
             }
 
-            mediaEstud = estudMaiorMedia(estuds.dados[indEstu]);
-            if(mediaEstud > maiorEstud){
-                maiorEstud = mediaEstud;
-                indEstudMaior = indEstu;
+            maiorMediaTemp = estudMaiorMedia(estuds.dados[indEstud]);
+            if(maiorMediaTemp > maiorMediaEstud){
+                maiorMediaEstud = maiorMediaTemp;
+                indEstudMaiorMedia = indEstud;
             }
 
-            mediaDisciplinas(estuds.dados[indEstu]);
+            mediaDisciplinas(estuds.dados[indEstud]);
+            for(int i = 0; i < qtasDisci; i++){
+                if((somasNotas[i] / estuds.qtosDados) < menorMediaDisci){
+                    indDisciMenorMedia = i;
+                }
+            }
         }
-        out.println("Disciplina com mais aprovacoes: " + disciplinas[indMaisAprov]);
-        out.println("Disciplina com mais retencoes: " + disciplinas[indMaisReten]);
-        out.println("Estudante com maior media:" + estuds.dados[indEstudMaior].getNome());
-        out.println("Maior nota do estudante com maior media: " + notaAlta(estuds.dados[indEstudMaior]));
-        out.println("Menor nota do estudante com maior media: " + notaBaixa(estuds.dados[indEstudMaior]));
-        for(int i = 0; i < qtasDisci; i++){
-            out.print("Media " + disciplinas[i] + ": ");
-            out.println(somasNotas[i] / estuds.qtosDados);
+        notaMaisAlta = 0;
+        notaMaisBaixa = 10;
+        for(int indEstud = 0; indEstud < estuds.qtosDados; indEstud++){
+            estudMenorNota(estuds.dados[indEstud], indDisciMenorMedia, indEstud);
+            estudMaiorNota(estuds.dados[indEstud], indDisciMenorMedia, indEstud);
+        }
+        out.printf("\nDisciplina com mais aprovações: %s", disciplinas[indMaisAprov]);
+        out.printf("\nDisciplina com mais retenções: %s\n", disciplinas[indMaisReten]);
+        out.printf("\nEstudante com maior média: %s", estuds.dados[indEstudMaiorMedia].getNome());
+        out.printf("\nMaior nota desse estudante: %f", notaAlta(estuds.dados[indEstudMaiorMedia]));
+        out.printf("\nMenor nota desse estudante: %f\n", notaBaixa(estuds.dados[indEstudMaiorMedia]));
+        for(int i = 0; i < qtasDisci; i++)
+            out.printf("\nMédia %s: %f", disciplinas[i], (somasNotas[i] / estuds.qtosDados));
+        out.printf("\nDisciplina com menor média: %s", disciplinas[indDisciMenorMedia]);
+        out.printf("\n%s teve a menor nota em %s", estuds.dados[indEstudMenorNota].getNome(), disciplinas[indDisciMenorMedia]);
+        out.printf("\n%s teve a maior nota em %s", estuds.dados[indEstudMaiorNota].getNome(), disciplinas[indDisciMenorMedia]);
 
-        }
+        out.print("\n\nTecle [Enter] para prosseguir: ");
+        leitor.nextLine();
     }
 
     private static void maisAprovacoes(Estudante estudAtual){
         double[] notaAtual = estudAtual.getNotas();
-        for(int indNotas=0; indNotas < estudAtual.getQuantasNotas(); indNotas++){
+        int qtasNotas = estudAtual.getQuantasNotas();
+        for(int indNotas=0; indNotas < qtasNotas; indNotas++){
             if(notaAtual[indNotas] >= 5)
                 maioresAprovacoes[indNotas]++;
         }
@@ -175,7 +191,8 @@ public class Manutencao {
 
     private static void maisRetencoes(Estudante estudAtual){
         double[] notaAtual = estudAtual.getNotas();
-        for(int indNotas=0; indNotas < estudAtual.getQuantasNotas(); indNotas++){
+        int qtasNotas = estudAtual.getQuantasNotas();
+        for(int indNotas=0; indNotas < qtasNotas; indNotas++){
             if(notaAtual[indNotas] < 5)
                 maioresRetencoes[indNotas]++;
         }
@@ -208,8 +225,25 @@ public class Manutencao {
 
     public static void mediaDisciplinas(Estudante estudAtual){
         double[] notas = estudAtual.getNotas();
-        for(int indNota = 0; indNota < qtasDisci; indNota++)
+        for(int indNota = 0; indNota < qtasDisci; indNota++){
             somasNotas[indNota] += notas[indNota];
+        }
+    }
+
+    public static void estudMenorNota(Estudante estudAtual, int indDisci, int indEstud){
+        double[] notas = estudAtual.getNotas();
+        if(notas[indDisci] <= notaMaisBaixa){
+            notaMaisBaixa = notas[indDisci];
+            indEstudMenorNota = indEstud;
+        }
+    }
+
+    private static void estudMaiorNota(Estudante estudAtual, int indDisci, int indEstud){
+        double[] notas = estudAtual.getNotas();
+        if(notas[indDisci] >= notaMaisAlta){
+            notaMaisAlta = notas[indDisci];
+            indEstudMaiorNota = indEstud;
+        }
     }
 
     public static void ordenarPorCurso() {
